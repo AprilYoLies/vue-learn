e92c192cdaa016bbf816b7ae6bfbef456880f322
eva <863821569@qq.com>
2022-05-20 23:08:41 +0800

    less配置初始化

--- src/main.js
+++ src/main.js
@@ -4,12 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false  // 关闭生产模式下给出的提示
 
 /* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+new Vue({ // 创建一个 Vue 的根实例
+  el: '#app', //挂载id,这个实例下所有的内容都会在index.html 一个id为app的div下显示
+  router, // 注入路由配置
+  components: { App },  //配置根模板 即打开页面显示那个组件
+  template: '<App/>'  // 注入组件
 })
