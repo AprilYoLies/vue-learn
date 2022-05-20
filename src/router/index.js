3a328088e7b712181240d8e58e3878af02be41af
eva <863821569@qq.com>
2022-05-21 00:39:40 +0800

    创建layout组件，并添加路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Layouts from '@/components/layouts' // 导入layouts.vue 组件
 
 Vue.use(Router)
 
-export default new Router({
+export default new Router({ // 创建 router 实例，然后传 `routes` 配置
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Layouts',
+      component: Layouts
     }
   ]
 })
