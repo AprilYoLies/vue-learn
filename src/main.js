60dad8f336e4cd15c09aa120a3d78841d2940fde
eva <863821569@qq.com>
2022-05-22 23:16:10 +0800

    用户登录模块 1、添加 vue 对于 scss 的支持； 2、添加 vue 对于 svg 的支持； 3、添加 vue 对于 store 的支持； 4、登录功能实现表单验证；

--- src/main.js
+++ src/main.js
@@ -3,14 +3,22 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import './plugins/element.js'
+import store from './store'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import '@/styles/index.scss' // global css
+import '@/permission'
+import '@/icons'
 
 Vue.config.productionTip = false  // 关闭生产模式下给出的提示
+
+Vue.use(ElementUI);
 
 /* eslint-disable no-new */
 new Vue({ // 创建一个 Vue 的根实例
   el: '#app', //挂载id,这个实例下所有的内容都会在index.html 一个id为app的div下显示
   router, // 注入路由配置
+  store,  // 使用 vuex
   components: { App },  //配置根模板 即打开页面显示那个组件
   template: '<App/>'  // 注入组件
 })
--- src/router/index.js
+++ src/router/index.js
@@ -6,9 +6,9 @@ Vue.use(Router)
 export default new Router({ // 创建 router 实例，然后传 `routes` 配置
   routes: [
     {
-      path: '/',
-      name: 'Vue',
-      component: Vue
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/login')
     }
   ]
 })
