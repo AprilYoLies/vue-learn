a060de81d2006289b0eea2f0bef21a81e4854eec
eva <863821569@qq.com>
2022-06-29 23:07:24 +0800

    完成数据列表及列表表头的制作

--- src/views/pms/product/index.vue
+++ src/views/pms/product/index.vue
@@ -66,6 +66,119 @@
         </el-form>
       </div>
     </el-card>
+    <el-card class="operate-container" shadow="never">
+      <i class="el-icon-tickets"></i>
+      <span>数据列表</span>
+      <el-button
+        class="btn-add"
+        @click=""
+        size="mini">
+        添加
+      </el-button>
+    </el-card>
+    <div class="table-container">
+      <el-table ref="productTable"
+                :data="list"
+                style="width: 100%"
+                @selection-change=""
+                v-loading=""
+                border>
+        <el-table-column type="selection" width="60" align="center"></el-table-column>
+        <el-table-column label="编号" width="100" align="center">
+          <template slot-scope="scope">{{scope.row.id}}</template>
+        </el-table-column>
+        <el-table-column label="商品图片" width="120" align="center">
+          <template slot-scope="scope"><img style="height: 80px" :src="scope.row.pic"></template>
+        </el-table-column>
+        <el-table-column label="商品名称" align="center">
+          <template slot-scope="scope">
+            <p>{{scope.row.name}}</p>
+            <p>品牌：{{scope.row.brandName}}</p>
+          </template>
+        </el-table-column>
+        <el-table-column label="价格/货号" width="120" align="center">
+          <template slot-scope="scope">
+            <p>价格：￥{{scope.row.price}}</p>
+            <p>货号：{{scope.row.productSn}}</p>
+          </template>
+        </el-table-column>
+        <el-table-column label="标签" width="140" align="center">
+          <template slot-scope="scope">
+            <p>上架：
+              <el-switch
+                @change="handlePublishStatusChange(scope.$index, scope.row)"
+                :active-value="1"
+                :inactive-value="0"
+                v-model="scope.row.publishStatus">
+              </el-switch>
+            </p>
+            <p>新品：
+              <el-switch
+                @change="handleNewStatusChange(scope.$index, scope.row)"
+                :active-value="1"
+                :inactive-value="0"
+                v-model="scope.row.newStatus">
+              </el-switch>
+            </p>
+            <p>推荐：
+              <el-switch
+                @change="handleRecommendStatusChange(scope.$index, scope.row)"
+                :active-value="1"
+                :inactive-value="0"
+                v-model="scope.row.recommandStatus">
+              </el-switch>
+            </p>
+          </template>
+        </el-table-column>
+        <el-table-column label="排序" width="100" align="center">
+          <template slot-scope="scope">{{scope.row.sort}}</template>
+        </el-table-column>
+        <el-table-column label="SKU库存" width="100" align="center">
+          <template slot-scope="scope">
+            <el-button type="primary" icon="el-icon-edit" @click="handleShowSkuEditDialog(scope.$index, scope.row)" circle></el-button>
+          </template>
+        </el-table-column>
+        <el-table-column label="销量" width="100" align="center">
+          <template slot-scope="scope">{{scope.row.sale}}</template>
+        </el-table-column>
+        <el-table-column label="审核状态" width="100" align="center">
+          <template slot-scope="scope">
+            <p>{{scope.row.verifyStatus | verifyStatusFilter}}</p>
+            <p>
+              <el-button
+                type="text"
+                @click="handleShowVerifyDetail(scope.$index, scope.row)">审核详情
+              </el-button>
+            </p>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" width="160" align="center">
+          <template slot-scope="scope">
+            <p>
+              <el-button
+                size="mini"
+                @click="handleShowProduct(scope.$index, scope.row)">查看
+              </el-button>
+              <el-button
+                size="mini"
+                @click="handleUpdateProduct(scope.$index, scope.row)">编辑
+              </el-button>
+            </p>
+            <p>
+              <el-button
+                size="mini"
+                @click="handleShowLog(scope.$index, scope.row)">日志
+              </el-button>
+              <el-button
+                size="mini"
+                type="danger"
+                @click="handleDelete(scope.$index, scope.row)">删除
+              </el-button>
+            </p>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
   </div>
 </template>
 
@@ -101,7 +214,8 @@ export default {
       }, {
         value: 0,
         label: '未审核'
-      }]
+      }],
+      list: null,
     }
   }
 }
